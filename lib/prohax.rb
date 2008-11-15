d973f87b48a9e0a81311fb4741f17d1b3c3b4538
Nick Partridge <nkpart@gmail.com>
2008-11-15 20:35:00 +1030

    Now with let in!

--- lib/prohax.rb
+++ lib/prohax.rb
@@ -1,6 +1,7 @@
-$:.unshift(File.dirname(__FILE__)) unless
-  $:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
+$: << File.expand_path(File.dirname(__FILE__))
 
 module Prohax
   VERSION = '1.0.0'
 end
+
+require 'prohax/let_in'
---
+++ spec/let_in_syntax_spec.rb
@@ -0,0 +1,19 @@
+require File.dirname(__FILE__) + '/spec_helper.rb'
+
+describe "The let().in {} syntax" do
+  it "should be let-like and in-ish" do
+    let(:foo => 5 ).in { foo }.should == 5
+  end
+
+  it "should be mad" do
+    x = 5
+    let(
+      :plus => proc { |a,b| a + b },
+      :times => proc { |a,b| a * b },
+      :five => proc { 5 },
+      :fast_five => 5
+    ).in { 
+      plus(x, times(five, plus(1, fast_five)))
+    }.should == 35
+  end
+end
