032f41f0d2cc27d0ff878493c986e87a5bea4935
Nick Partridge <nkpart@gmail.com>
2008-11-16 11:56:23 +1030

    cleaned up let-in, removed newgem dependency

--- lib/prohax/let_in.rb
+++ lib/prohax/let_in.rb
@@ -1,32 +1,30 @@
-module Kernel  
-  
-  class LetProxy
-    class Container
-      # TODO undefine a lot of shiz
-      def initialize defines
-        defines.each do |key, value|
-          if value.respond_to? :call then
-            self.class.class_eval {
-              define_method(key) { |*args| value.call *args }
-            }
-          else
-            self.class.class_eval {
-              define_method(key) { value }
-            }
-          end
-        end
+class Container
+  def initialize defines
+    defines.each do |key, value|
+      if value.respond_to? :call then
+        self.class.class_eval {
+          define_method(key) { |*args| value.call *args }
+        }
+      else
+        self.class.class_eval {
+          define_method(key) { value }
+        }
       end
     end
-    
-    def initialize defines
-      @defines = defines
-    end
-    
-    def in &blk
-      Container.new(@defines).instance_eval &blk
-    end
   end
+end
   
+class LetProxy
+  def initialize defines
+    @defines = defines
+  end
+
+  def in &blk
+    Container.new(@defines).instance_eval &blk
+  end
+end
+
+module Kernel    
   def let defines
     LetProxy.new(defines)
   end
